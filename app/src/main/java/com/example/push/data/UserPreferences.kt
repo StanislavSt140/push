ebeb8e4229030ff556ac62430cb7a29f7d7c0cfe
Staspropisnov58 <Staspropisnov58@gmail.com>
2025-05-01 16:39:30 +0300

    Add users

--- app/src/main/java/com/example/push/data/UserPreferences.kt
+++ app/src/main/java/com/example/push/data/UserPreferences.kt
@@ -5,15 +5,17 @@ import android.content.Context
 class UserPreferences(context: Context) {
     private val prefs = context.getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
 
-    fun saveUser(id: Int, name: String, role: String) {
+    fun saveUser(id: Int, name: String,className: String, role: String) {
         prefs.edit()
             .putInt("user_id", id)
             .putString("name", name)
             .putString("role", role)
+            .putString("className", className)
             .apply()
     }
 
     fun getUserId(): Int = prefs.getInt("user_id", -1)
     fun getUserName(): String = prefs.getString("name", "Гість") ?: "Гість"
     fun getUserRole(): String = prefs.getString("role", "Без ролі") ?: "Без ролі"
+    fun getUserClass(): String = prefs.getString("className", "Без класу") ?: "Без класу"
 }
